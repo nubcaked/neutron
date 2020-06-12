60a998458c09124a785be04eda56ac47852b8808
nubcaked <37624241+nubcaked@users.noreply.github.com>
2020-06-12 18:45:57 +0800

    Added Navbar with logo | Added Home component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/layouts/Navbar.js';
+import Home from './components/routes/Home.js';
+import NeutronRectangleLogo from './images/neutron-rectangle-logo.png';
+import Container from '@material-ui/core/Container';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App">
+      <BrowserRouter>
+        <Navbar logo={NeutronRectangleLogo} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
+    </Container>
   );
 }
 
---
+++ src/components/routes/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+
+function Home() {
+  return (
+    <Grid container justify="center">
+      <Grid item>
+        HI
+      </Grid>
+      <Grid item>
+        HI
+      </Grid>
+      <Grid item>
+        HI
+      </Grid>
+    </Grid>
+  );
+}
+
+export default Home;
