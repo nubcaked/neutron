2233878e4e7429b5210d391ac1667669733cfe2c
nubcaked <37624241+nubcaked@users.noreply.github.com>
2020-06-12 23:41:28 +0800

    Added firebase hosting configs

--- src/components/routes/Home.js
+++ src/components/routes/Home.js
@@ -4,15 +4,10 @@ import Grid from '@material-ui/core/Grid';
 function Home() {
   return (
     <Grid container justify="center">
-      <Grid item>
-        HI
-      </Grid>
-      <Grid item>
-        HI
-      </Grid>
-      <Grid item>
-        HI
-      </Grid>
+      <h1>Need a custom-built web app or website?</h1>
+      <h1>Need help with a programming assignment?</h1>
+      <h1>Or simply need a tutor to help with a last minute preparation for a technical interview?</h1>
+      <h1>Fear not! Because NeutronSG is here to help you with your tech problems.</h1>
     </Grid>
   );
 }
